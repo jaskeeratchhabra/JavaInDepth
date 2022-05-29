04c076c1a51625a76fada820650794259fc0a590
jaskeeratchhabra <jkchhabra99@gmail.com>
2022-05-29 23:48:37 +0530

    push

--- LearnInDepth/src/unit_03/P15_FileHandling_In_Java.java
+++ LearnInDepth/src/unit_03/P15_FileHandling_In_Java.java
@@ -1,5 +1,11 @@
 package unit_03;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 public class P15_FileHandling_In_Java {
 
 	public static void main(String[] args) {
@@ -8,4 +14,37 @@ public class P15_FileHandling_In_Java {
 
 	}
 
+}
+class ABCDEF{
+	static int a =10;
+	void takeInputFromAFile() throws IOException{
+		String path="C:\\Users\\jkchh\\git\\JavaInDepth\\JavaInDepth\\LearnInDepth\\src\\unit_03\\input.txt";
+		File file =new File(path);
+		FileReader obj=new FileReader(file);
+		BufferedReader br=new BufferedReader(obj) ;
+		String st;
+		List<String> listOfStrings =new ArrayList<String>();
+		
+		//read entire line of a string
+		String line=br.readLine();
+		while(line!=null)
+		{
+			listOfStrings.add(line);
+			line =br.readLine();
+		}
+		listOfStrings.remove(0);
+		
+		for(String str:listOfStrings) {
+			System.out.println(str);
+		}
+		System.out.println("\n");
+
+		br.close();//discuss in miultithreading
+	}
+	//how to create log using java
+	void saveOutputIntoAFile()throws IOException
+	{
+		FileWriter myObj=null;
+		
+	}
 }
